e9a2bc9a6bba0e915da90fce0530cf2ae7512e0e
Nils Cambreleng <nilscambreleng@gmail.com>
2024-02-04 01:23:30 +0100

    disable session replay

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -14,12 +14,11 @@ Sentry.init({
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
-  replaysOnErrorSampleRate: 1.0,
+  // replaysOnErrorSampleRate: 1,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 1,
-  
+  replaysSessionSampleRate: 0,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
